8a4d39a02747cc9963c6c9cdb9102fd5ef704606
shellteo <shellteo@163.com>
2017-12-12 15:19:32 +0800

    update send mail

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,39 @@
-var express = require('express');
-var router = express.Router();
-var nodemailer = require('nodemailer');
+let express = require('express');
+let router = express.Router();
+let nodemailer = require('nodemailer');
 
-var mailTransport = nodemailer.createTransport({
+// 开启一个 SMTP 连接池
+let transporter = nodemailer.createTransport({
     host: 'smtp.163.com',
-    secureConnection: true, // 使用SSL方式（安全方式，防止被窃取信息）
+    secureConnection: true, // use SSL
+    port: 465,
+    secure: true, // secure:true for port 465, secure:false for port 587
     auth: {
         user: 'shellteo@163.com',
         pass: 'Zx1994'
-    },
+    }
 });
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
-/* 浏览器输入地址（如127.0.0.1:3000/sned）后即发送 */
+/* 浏览器输入地址（如127.0.0.1:3000/send）后即发送 */
 router.get('/send', function(req, res, next) {
-    var options = {
-        from        : '"shell" <shellteo@163.com>',
-        to          : '767070256@qq.com, 825606393@qq.com',
-        // cc         : ''  //抄送
-        // bcc      : ''    //密送
-        subject        : '一封来自Node Mailer的邮件',
-        text          : '一封来自Node Mailer的邮件',
-        html           : '<h1>你好，这是一封来自NodeMailer的邮件！</h1><p><img src="cid:00000001"/></p>'
-        /*attachments :
-            [
-                {
-                    filename: 'img1.png',            // 改成你的附件名
-                    path: 'public/images/img1.png',  // 改成你的附件路径
-                    cid : '00000001'                 // cid可被邮件使用
-                },
-                {
-                    filename: 'img2.png',            // 改成你的附件名
-                    path: 'public/images/img2.png',  // 改成你的附件路径
-                    cid : '00000002'                 // cid可被邮件使用
-                },
-            ]*/
+    let options = {
+        from: '"张翔" <shellteo@163.com>',
+        to: '767070256@qq.com, 825606393@qq.com',
+        subject: 'node mailer测试发送',
+        text: '测试',
+        html: '<p>测试html</p>'
     };
 
-    mailTransport.sendMail(options, function(err, msg){
+    transporter.sendMail(options, function(err, msg){
         if(err){
             console.log(err);
             res.render('mail', { title: err });
-        }
-        else {
+        }else {
             console.log(msg);
             res.render('mail', { title: "已接收："+msg.accepted});
         }
